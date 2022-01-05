ecd13edb2154fbfdca8e42a7b362289866ed9c04
suryans66 <nssuryaj@gmail.com>
2022-01-06 00:54:03 +0530

    From create to master

--- test.java
+++ test.java
@@ -1,13 +1,14 @@
 public class test {
     public static void main(String[] args) {
-        
+
         System.out.println("Hello Git");
-        
+
         int num1 = 10;
         int num2 = 20;
         int sum = num1 + num2;
         System.out.println(sum);
 
+        System.out.println("Incoming another Branch");
         System.out.println("Hello Intellij");
     }
 }
